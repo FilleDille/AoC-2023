7df11e89372a7e0526f11fbfe86b2a46044548f5
engstfi <filip.engstrom@nordax.se>
2023-12-01 12:13:13 +0100

    day 1 part 2 done

--- day/01/src/part_2.py
+++ day/01/src/part_2.py
@@ -13,10 +13,13 @@ num_dict = {
         'nine': '9'
     }
 
+pattern = r'(?=(one|two|three|four|five|six|seven|eight|nine))'
+
 
 def run(inp) -> int:
     transformed_inp = [transform(x) for x in inp]
-    inp_int_only = [''.join(re.findall('[0-9]', x)) for x in transformed_inp]
+    translated_inp = [translate(x) for x in transformed_inp]
+    inp_int_only = [''.join(re.findall('[0-9]', x)) for x in translated_inp]
     inp_two_ints = [int(x[0] + x[-1]) for x in inp_int_only]
     sum_calibration_values = sum(inp_two_ints)
     return sum_calibration_values
@@ -24,14 +27,29 @@ def run(inp) -> int:
 
 def transform(inp_line: str) -> str:
     transformed_line = inp_line
-    pattern = r'(?=(one|two|three|four|five|six|seven|eight|nine))'
-    regex_search = re.findall(pattern, transformed_line)
+    regex_search = re.finditer(pattern, transformed_line)
+    span_list = []
 
     for found_match in regex_search:
-        transformed_line = transformed_line.replace(found_match, num_dict[found_match])
+        keyword = found_match.group(1)
+        start_pos = found_match.start()
+        span_list.append((keyword, start_pos, start_pos + len(keyword) - 1))
 
-    while regex_search:
-        transformed_line = transformed_line.replace(regex_search.group(), num_dict[regex_search.group()])
-        regex_search = re.search(pattern, transformed_line)
+    for i in range(len(span_list) - 1):
+        if span_list[i][2] >= span_list[i + 1][1]:
+            first_part_of_overlap = span_list[i][0]
+            last_char = first_part_of_overlap[-1]
+            edited_first_part = ''.join((first_part_of_overlap, last_char))
+            transformed_line = transformed_line.replace(first_part_of_overlap, edited_first_part)
 
     return transformed_line
+
+
+def translate(inp_line: str) -> str:
+    translated_line = inp_line
+
+    for k, v in num_dict.items():
+        if k in translated_line:
+            translated_line = translated_line.replace(k, v)
+
+    return translated_line
